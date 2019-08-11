f38db9db9d6ac035c603619252aeb676dfd10171
Kyle Wuellner <rockstar81@comcast.net>
2019-08-10 19:07:54 -0500

    added band search

---
+++ liri.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+var inquirer = require('inquirer');
+var axios = require("axios");
+var moment = require("moment");
+var fs = require("fs");
+var keys = require("./keys.js");
+
+
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify)
+
+var search = process.argv[2];
+var input = process.argv[3];
+
+// beginning questions
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is your name?"
+//     },
+
+//     {
+//         type: "list",
+//         name: "whichApp",
+//         message: "Which application would you like to run?",
+//         choice: ["Spotify", "OMBD", "BandsInTown"]
+//     },
+// ])
+// .then(function (user) {
+
+//concert-this
+function concertThis(artist) {
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    request(queryURL, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            var concertInfo = JSON.parse(body);
+
+            var dayTime = concertInfo[0].datetime
+            var momentDT = moment().format('L');
+
+            console.log("======================");
+            console.log("Venue : " + concertInfo[0].venue.name +
+                "\nLocation : " + concertInfo[0].venue.city + "," + concertInfo[0].venue.state +
+                "\nDate of the Event : " + momentDT + "\n======================");
+        };
+    });
+
+}
